b1d785ecb51a12233ef660e96be620ca7806a46b
julio <julio.cesar.2000@hotmail.com>
2025-06-19 21:26:46 -0400

    feat: implement auth protection for officer routes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,33 @@
 import "@/styles/globals.css";
 
-export default function OfficerLayout({ children }: { children: React.ReactNode }) {
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+export const metadata: Metadata = {
+  title: "CornerStone - Signin",
+  description: "Signin",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-      <html lang="en" >
-          <body className="min-h-screen flex flex-col min-w-0 lg:flex-row w-full transition-all duration-300">
-              {children}
-          </body>
-      </html>
+    <html lang="en" className={`${geist.variable}`}>
+      <body className="min-h-screen bg-gray-50 font-sans antialiased">
+        <TRPCReactProvider>
+          <main className="flex min-h-screen items-center justify-center px-4 sm:px-6">
+            {children}
+          </main>
+        </TRPCReactProvider>
+      </body>
+    </html>
   );
 }
--- src/app/(officer)/layout.tsx
+++ src/app/(officer)/layout.tsx
@@ -1,6 +1,8 @@
 import AppSideBar from "@/features/shared/AppSideBar/AppSideBar";
 import AppTopNav from "@/features/shared/AppTopNav/TopNav/AppTopNav";
 import "@/styles/globals.css";
+import { TRPCReactProvider } from "@/trpc/react";
+import { SessionProvider } from "next-auth/react";
 
 export default function OfficerLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,7 +10,11 @@ export default function OfficerLayout({ children }: { children: React.ReactNode 
           <body className="min-h-screen flex flex-col min-w-0 lg:flex-row w-full transition-all duration-300">
               <AppSideBar />
               <AppTopNav />
-              {children}
+              <SessionProvider>
+                <TRPCReactProvider>
+                  {children}
+                </TRPCReactProvider>
+              </SessionProvider>
           </body>
       </html>
   );
