c8fcc7cdade140ab65b25966c13c50f7fc7fdf63
v4nishp01nt <shagasresthaa@gmail.com>
2025-09-15 11:33:40 -0400

    added destination deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd webdev-landing
+
+# Pull Latest Code from repo just in case of updated deploy script/docker-compose script
+git pull origin production
+
+# Stop running services
+docker compose down
+
+# Pull latest image
+docker pull devclubwmu/devclub-prod:webdev-landing
+
+# Start with latest image in background
+docker compose up -d
